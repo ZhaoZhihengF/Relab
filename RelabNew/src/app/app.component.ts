5daf7650f60a7b8063a00b083d86d176baf3570d
ZhaoZhihengF <zhao.zhiheng@iisgalvanimi.edu.it>
2021-10-01 09:54:53 +0000

    q

--- RelabNew/src/app/app.component.ts
+++ RelabNew/src/app/app.component.ts
@@ -11,11 +11,28 @@ export class AppComponent {
   center : any;
   position : any;
   label :string;
+  circleOptions : any;
+  circleOptions1 : any;
+  markerOptions: google.maps.MarkerOptions;
+ 
   constructor()
   {
     this.center={lat: 45.506738, lng: 9.190766};
     this.position = this.center;
     this.label = "ciao";
+    this.circleOptions = {fillColor : 'red'}
+    this.circleOptions1 = {fillColor : 'Blue'}
+    let iconData : google.maps.Icon = 
+      {
+      url:'./assets/img/cat_acrobat.ico',
+      scaledSize : new google.maps.Size(60,60)
+      }
+    this.markerOptions = {icon:iconData}
   }
-
+  Red()
+  {this.circleOptions1 = {fillColor : 'Red'}}
+  Yellow()
+  {this.circleOptions1 = {fillColor : 'Yellow'}}
+  Green()
+  {this.circleOptions1 = {fillColor : 'Green'}}
 }
